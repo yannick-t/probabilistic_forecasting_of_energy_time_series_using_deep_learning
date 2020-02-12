998ed7c9f217c73b1df25f04c01d0313f8188482
yannick <yannick.tanner@wagenbacherhof.de>
2020-02-12 17:25:52 +0100

    some short term forecasting features (values from 1, 2, 3h, and 15, 30, 45min), refactoring

---
+++ code/training/training_util.py
@@ -0,0 +1,18 @@
+import time
+
+def load_train(model, x_train, y_train, model_name, model_folder, model_prefix, load_saved):
+    model_file = model_folder + model_prefix + model_name
+
+    if load_saved:
+        model.initialize()
+        model.load_params(model_file)
+
+        return 0
+    else:
+        start = time.time_ns()
+        model.fit(x_train, y_train)
+        end = time.time_ns()
+        print('fit time ' + model_name + ' %d ns' % (end - start))
+        model.save_params(model_file)
+
+        return end - start
